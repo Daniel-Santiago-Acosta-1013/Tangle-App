99843d7b5e9a461b79f1156cee60fd8b18dff086
Daniel-Santiago-Acosta-1013 <santiagoacosta1013@gmail.com>
2023-09-12 14:01:59 -0500

    [add][fix] adding initial logic for all list

--- App.tsx
+++ App.tsx
@@ -1,11 +1,59 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, StyleSheet, TextInput, Button, FlatList } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function App() {
+  const [task, setTask] = useState<string>(''); // añadido tipo aquí también por consistencia
+  const [tasks, setTasks] = useState<string[]>([]);
+
+  useEffect(() => {
+    loadTasks();
+  }, []);
+
+  const loadTasks = async () => {
+    try {
+      const savedTasks = await AsyncStorage.getItem('tasks');
+      if (savedTasks) setTasks(JSON.parse(savedTasks));
+    } catch (error) {
+      console.error("Error loading tasks", error);
+    }
+  };
+
+  const addTask = async () => {
+    if (task) {
+      const newTasks = [...tasks, task];
+      setTasks(newTasks);
+      setTask('');
+      await AsyncStorage.setItem('tasks', JSON.stringify(newTasks));
+    }
+  };
+
+  const deleteTask = async (index: number) => {
+    const newTasks = tasks.filter((_, i) => i !== index);
+    setTasks(newTasks);
+    await AsyncStorage.setItem('tasks', JSON.stringify(newTasks));
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Todo List</Text>
+      <TextInput
+        value={task}
+        onChangeText={setTask}
+        style={styles.input}
+        placeholder="Enter a task..."
+      />
+      <Button title="Add Task" onPress={addTask} />
+      <FlatList
+        data={tasks}
+        keyExtractor={(item, index) => index.toString()}
+        renderItem={({ item, index }) => (
+          <View style={styles.task}>
+            <Text>{item}</Text>
+            <Button title="Delete" onPress={() => deleteTask(index)} />
+          </View>
+        )}
+      />
     </View>
   );
 }
@@ -13,8 +61,26 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    padding: 20,
+    backgroundColor: '#f5f5f5',
+  },
+  title: {
+    fontSize: 24,
+    marginBottom: 10,
+  },
+  input: {
+    padding: 10,
+    marginBottom: 10,
+    backgroundColor: 'white',
+    borderRadius: 5,
+  },
+  task: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
     alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'white',
+    padding: 10,
+    borderRadius: 5,
+    marginBottom: 10,
   },
 });
