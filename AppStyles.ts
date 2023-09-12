544a1233cfea187f4fff4f75c0f4189499a2c584
Daniel-Santiago-Acosta-1013 <santiagoacosta1013@gmail.com>
2023-09-12 17:56:57 -0500

    [add][fix] modularizing the types of styles

--- AppStyles.ts
+++ AppStyles.ts
@@ -1,27 +1,5 @@
 import { StyleSheet } from "react-native";
-
-interface StylesProps {
-  container: object;
-  title: object;
-  inputContainer: object;
-  input: object;
-  addButton: object;
-  addButtonText: object;
-  task: object;
-  taskTextContainer: object;
-  taskText: object;
-  completedTask: object;
-  deleteButton: object;
-  deleteButtonText: object;
-  prioritySelector: object;
-  priorityButton: object;
-  activePriority: object;
-  priorityButtonText: object;
-  high: object;
-  medium: object;
-  low: object;
-  [key: string]: any;
-}
+import StylesProps from "./types/styles.types";
 
 const styles: StylesProps = StyleSheet.create({
   container: {
---
+++ types/styles.types.ts
@@ -0,0 +1,24 @@
+interface StylesProps {
+    container: object;
+    title: object;
+    inputContainer: object;
+    input: object;
+    addButton: object;
+    addButtonText: object;
+    task: object;
+    taskTextContainer: object;
+    taskText: object;
+    completedTask: object;
+    deleteButton: object;
+    deleteButtonText: object;
+    prioritySelector: object;
+    priorityButton: object;
+    activePriority: object;
+    priorityButtonText: object;
+    high: object;
+    medium: object;
+    low: object;
+    [key: string]: any;
+  }
+  
+  export default StylesProps;
